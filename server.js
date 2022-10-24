b52f364abdb923dbafdfc5766f53f14eb93618f5
Jorim Soika <jorim.soika@gmx.de>
2022-10-24 15:52:31 +0800

    edited static file middleware

--- server.js
+++ server.js
@@ -9,7 +9,6 @@ const morgan = require('morgan');
 
 
 //Middleware
-app.use(express.static(path.join(__dirname, 'styles')));
 app.use(express.static(__dirname));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
