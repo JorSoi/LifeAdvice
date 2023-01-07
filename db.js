ddbe6758d7a595f285d43a0c797dcd502a9f9ac1
Jorim Soika <jorim.soika@gmx.de>
2023-01-07 10:46:30 +0100

    test

--- db.js
+++ db.js
@@ -6,11 +6,11 @@ require('dotenv').config({
 
 const Pool = require('pg').Pool;
 const pool = new Pool({
-    host: process.env.PG_HOST,
-    database: process.env.PG_DATABASE,
-    user: process.env.PG_USER,
-    port: process.env.PG_PORT,
-    password: 'v3UGnWiJ1p0okLZVPCor'
+    host: 'containers-us-west-96.railway.app',
+    database: 'railway',
+    user: 'postgres',
+    port: 7832,
+    password: 'v3UGnWiJ1p0okLZVPCor',
     // ssl: {
     //     rejectUnauthorized: false //In production SSL Encryption is deactivated to access remote database
     // }
